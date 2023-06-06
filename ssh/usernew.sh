512dc9eee6ab086c3d32c5f18b37b73234d6754b
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-06-06 14:05:27 +0700

    Update usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -85,6 +85,7 @@ echo -e "SSH-SSL-WS  : 443" | tee -a /etc/log-create-user.log
 echo -e "SSL/TLS     : 447 , 777" | tee -a /etc/log-create-user.log
 echo -e "UDPGW       : 7100-7300" | tee -a /etc/log-create-user.log
 echo -e "SSH-UDP     : 1-65535" | tee -a /etc/log-create-user.log
+echo -e "SETING-UDP  : $domen:1-65350@$Login:$Pass"
 #echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 #echo -e "OpenVPN Config : http://$IP:81/" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log

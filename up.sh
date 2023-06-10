380116397f80ff10a6b052f199f999c4d7e82054
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-06-10 21:39:30 +0700

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# ==========================================
+# Color
+# hapus menu
+rm -rf menu
+rm -rf ipsaya
+rm -rf sl-fix
+rm -rf sshovpnmenu
+rm -rf l2tpmenu
+rm -rf pptpmenu
+rm -rf sstpmenu
+rm -rf wgmenu
+rm -rf ssmenu
+rm -rf ssrmenu
+rm -rf vmessmenu
+rm -rf vlessmenu
+rm -rf grpcmenu
+rm -rf grpcupdate
+rm -rf trmenu
+rm -rf trgomenu
+rm -rf setmenu
+rm -rf slowdnsmenu
+rm -rf running
+rm -rf copyrepo
